c8fe308b0c690d6a9bf3c12be19e97bcb3d50564
Dearreck <dearreck.escoto@gmail.com>
2022-06-06 16:28:24 -0600

    Datos Personales

--- main.js
+++ main.js
@@ -11,36 +11,49 @@ function ShowUser() {
     .then(response => response.json())
     .then(result => {
 
+      // Obtener el objeto con los datos
+      person = result.results[0];
+
       // Foto y Nombre Completo
       contenedor.innerHTML = `
         <picture id="logo">
-          <source media="(min-width: 768px)" srcset="${result.results[0].picture.large}">
-          <source media="(min-width: 480px)" srcset="${result.results[0].picture.medium}">
-          <img src="${result.results[0].picture.thumbnail}" alt="">
+          <source media="(min-width: 768px)" srcset="${person.picture.large}">
+          <source media="(min-width: 480px)" srcset="${person.picture.medium}">
+          <img src="${person.picture.thumbnail}" alt="">
         </picture>
-        <h3>${result.results[0].name.first} ${result.results[0].name.last}</h3>
+        <h3>${person.name.first} ${person.name.last}</h3>
       `;
       // Cambiar el fondo
-      if (result.results[0].gender == "female") {
+      if (person.gender == "female") {
         contenedor.style.backgroundColor = "pink";
         contenedor.style.color = "black";
       }
 
+      datos.innerHTML = `
+        <label>Datos Personales</label>
+        <label>Nombre Completo: ${person.name.title} ${person.name.first} ${person.name.last}</label>
+        <label for="dob">Fecha de Nacimiento: <input type="date" name="dob" id="dob" value="${person.dob.date.slice(0, 10)}" readonly></label>
+        <br/><label>Datos de Contacto</label>
+        <label>e-mail: ${person.email}</label>
+        <label>Teléfono: ${person.phone}</label>
+        <label>Celular: ${person.cell}</label>
+      `;
+
       // Nacionalidad: Bandera
       nacionalidad.innerHTML = `
       <picture id="flag">
-        <source media="(min-width: 480px)" srcset="https://raw.githubusercontent.com/lipis/flag-icons/main/flags/4x3/${result.results[0].nat.toLowerCase()}.svg">
-        <img src="https://raw.githubusercontent.com/lipis/flag-icons/main/flags/1x1/${result.results[0].nat.toLowerCase()}.svg" alt="">
+        <source media="(min-width: 480px)" srcset="https://raw.githubusercontent.com/lipis/flag-icons/main/flags/4x3/${person.nat.toLowerCase()}.svg">
+        <img src="https://raw.githubusercontent.com/lipis/flag-icons/main/flags/1x1/${person.nat.toLowerCase()}.svg" alt="">
       </picture>
-      <label>País: ${result.results[0].location.country}</label>
-      <label>Estado: ${result.results[0].location.state}</label>
-      <label>Ciudad: ${result.results[0].location.city}</label>
-      <label>Dirección: ${result.results[0].location.street.number} ${result.results[0].location.street.name}</label><br/>
+      <label>País: ${person.location.country}</label>
+      <label>Estado: ${person.location.state}</label>
+      <label>Ciudad: ${person.location.city}</label>
+      <label>Dirección: ${person.location.street.number} ${person.location.street.name}</label><br/>
       `;
 
       // Mostrar el mapa: Somoto (13.479576, -86.586356)
-      let latit = Number(result.results[0].location.coordinates.latitude)
-      let longit = Number(result.results[0].location.coordinates.longitude)
+      let latit = Number(person.location.coordinates.latitude)
+      let longit = Number(person.location.coordinates.longitude)
       var map = L.map('map').setView([latit, longit], 3);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -52,7 +65,7 @@ function ShowUser() {
         .openPopup();
 
 
-      console.log(result.results[0]);
+      console.log(person);
 
     })
     .catch(error => console.log('error', error));
